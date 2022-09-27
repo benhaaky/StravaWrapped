ec064b98675cc659fd7594c4052ebfc62c35cbac
Ben Haak <benhaak@Bens-MacBook-Air.local>
2022-09-27 16:53:30 +0200

    Link to strava authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Stava Wrapped
         </p>
         <a
           className="App-link"
@@ -15,11 +15,23 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+
         </a>
+        <div>
+            <button onClick={handleLogin}>Connect with Strava</button>
+          </div>
       </header>
     </div>
   );
 }
+const { REACT_APP_CLIENT_ID } = process.env;
+const clientId = "94320"
+const redirectUrl = "http://localhost:3000/redirect";
+const responseType = "code";
+const approvalPrompt = "auto";
+const scope = "activity:read_all";
 
+const handleLogin = () => {
+    window.location = `http://www.strava.com/oauth/authorize?client_id=${clientId}&response_type=${responseType}&redirect_uri=${redirectUrl}/exchange_token&approval_prompt=auto&scope=${scope}`;
+};
 export default App;
